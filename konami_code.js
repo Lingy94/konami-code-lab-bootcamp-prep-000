a47b7e4c277756a5a514b39356d91c28e24dbe8b
Jack Ling <j.lingyx@gmail.com>
2018-10-02 23:51:10 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,24 @@ const codes = [
   "a"
 ];
 
+var konamiCodePos = 0;
+
 function init() {
-  // your code here
+
+  document.addEventListener('keydown', function(e) { 
+    const key = e.key;
+    for (i = 0; i < codes.length; i++) {
+      if (key == codes[i]) {
+        konamiCodePos++;
+        if (konamiCodePos === codes.length){
+        alert("Konami Code!");
+        konamiCodePos = 0;
+        }
+      }
+      else {
+        konamiCodePos = 0;
+      }
+      }
+    }
+    );
 }
